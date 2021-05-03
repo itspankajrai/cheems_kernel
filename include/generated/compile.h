d298d119867543b0e26d548a000307cc0c2f0510
pankaj <pankajrai@live.com>
2021-05-03 13:22:34 +0530

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Mon May 3 02:13:10 IST 2021"
+#define LINUX_COMPILE_BY "rai"
+#define LINUX_COMPILE_HOST "LAPTOP-RLS9E1RI"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,2 @@
+#define UTS_RELEASE "3.4.42-Cheems-g39a734e6-dirty"
+#define SHORT_UTS_RELEASE "3.4.42-Cheems"
